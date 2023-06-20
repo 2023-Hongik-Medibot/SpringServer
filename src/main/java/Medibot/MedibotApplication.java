1807a03e910e1347831348a02a0bc0845a3c968d
dlwlals1289 <dlwlals1289@gmail.com>
2023-06-21 00:37:22 +0900

    fix : cors error fixing2

--- src/main/java/Medibot/MedibotApplication.java
+++ src/main/java/Medibot/MedibotApplication.java
@@ -2,8 +2,9 @@ package Medibot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class MedibotApplication {
 
 	public static void main(String[] args) {
